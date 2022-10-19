91d3ff52ad15b60d50cf42e4891702079e87444a
Azhar <Azhar@DESKTOP-3MHP04M>
2022-10-19 15:07:12 +0500

    completed

--- OOPproject/src/Project.java
+++ OOPproject/src/Project.java
@@ -5,26 +5,43 @@ class Display extends Thread{
 	 * specific years
 	 */
 	public void run() {
+		Scanner sc=new Scanner(System.in);
 		/*run method contains calculations for interest dealings and will
 		 * tell user that how many years they will have to wait for their desired
 		 *  amount.
 		 */
 		while(true) {
-		Scanner sc=new Scanner(System.in);
-		int years = 0;
-		System.out.println("Enter your amount: ");
-	    double i=sc.nextDouble();
-	    System.out.println("Enter your amount: ");
-	    double j=sc.nextDouble();
-	    while (i < j) {
-	    	double interest = i * .075;
-	        i += interest;
-	        years ++;
-	    }//end of while loop
-
-	    System.out.println("It will take " + years + " years for "+i+" investment to be worth at least "
-	    		+j);
-	    System.out.println("Enter "+ "y"+" to continue and "+ "n" +" to quite");
+		System.out.println("press 1 for simple interest"+" OR press 2 for compound interest : ");
+		int i=sc.nextInt();
+	    if(i==1) {
+	    	System.out.println("Enter the principal amount: ");
+	    	double p=sc.nextDouble();
+	    	System.out.println("Enter the rate : ");
+	    	double r=sc.nextDouble();
+	    	System.out.println("Enter the time: ");
+	    	double t=sc.nextDouble();
+	    	double sinterest=(p*r*t)/100;
+	    	System.out.println("The simple interest will be "+sinterest+" of "+(int)t+" years");
+	    	}
+	    else if(i==2) {
+	    	System.out.println("Enter the principal amount: ");
+	    	double p=sc.nextDouble();
+	    	System.out.println("Enter the rate : ");
+	    	double r=sc.nextDouble();
+	    	System.out.println("Enter the time: ");
+	    	double t=sc.nextDouble();
+	    	System.out.println("Enter number of time interest will be compounded : ");
+	    	int no=sc.nextInt();
+	    	double cinterest=p*(Math.pow((1+ r/no),(t*no)));
+	    	
+	    	System.out.println("The compound interest will be "+cinterest+" of "+(int)t+" years");
+	    	
+	    }
+	    else {
+	    	System.out.println("WRONG CHOICE!");
+	    }
+		
+	    System.out.println("Enter "+ "y"+" to continue and "+ "n" +" to go back to the menue");
 		char p=sc.next().charAt(0);
 		if(p=='y') {
 			continue;
@@ -46,18 +63,19 @@ class shareInvestment extends Thread{
 		 * market price.
 		 */
 		Scanner sc=new Scanner (System.in);
+		System.out.println("***WELCOME TO STOCK EXCHANGES***");
 		System.out.println("Enter your buying price per share : ");
-		double buyingprice=sc.nextDouble();
+		long buyingprice=sc.nextLong();
 		int day=1;
 		
 		while(true) {
 			System.out.println("Enter your closing price for day : "+day+""
 					+ " (any negative value to leave)");
-			double closingprice=sc.nextDouble();
+			long closingprice=sc.nextLong();
 			if(closingprice<0.0) {
 				break;
 			}
-			double earning=closingprice-buyingprice;
+			long earning=closingprice-buyingprice;
 			if(earning>0.0) {
 				System.out.println("After day "+day+" your earning  "+earning+
 						"RS per share ");
@@ -70,7 +88,7 @@ class shareInvestment extends Thread{
 					System.out.println("After day "+day+" you have not eanrings per share ");
 				}
 			day+=1;
-			System.out.println("Enter "+ "y"+" to continue and "+ "n" +" to quite");
+			System.out.println("Enter "+ "y"+" to continue and "+ "n" +" to go back to the menue");
 			char p=sc.next().charAt(0);
 			if(p=='y') {
 				continue;
@@ -80,7 +98,7 @@ class shareInvestment extends Thread{
 			}
 			
 		}//end of while loop
-		sc.close();
+	
 	}// end of run method
 	}//end of class
 
@@ -115,7 +133,7 @@ class propertyInvestment extends Thread{
 			System.out.println("WRONG INPUT!");
 		}
 		i+=1;
-		System.out.println("Enter "+ "y"+" to continue and "+ "n" +" to quite");
+		System.out.println("Enter "+ "y"+" to continue and "+ "n" +" to go back to the menue");
 		char p=sc.next().charAt(0);
 		if(p=='y') {
 			continue;
@@ -128,14 +146,70 @@ class propertyInvestment extends Thread{
 	}// end of run method
  
 }// end of class
-public class Project{
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("********WELCOME TO INVESTMENT DEALINGS********");
+class bitcoinInvestment extends Thread{
+	public void run() {
+		Scanner sc=new Scanner (System.in);
 		while(true) {
-		System.out.println("Press 1 for share investments "
-				+ "press 2 for property investments"+
-				"press 3 for knowing your interest rate");
+		System.out.println("Enter current rate of 1 BTC : ");
+	    double bt=sc.nextDouble();
+	    System.out.println("Enter your investment : ");
+	    double nt=sc.nextDouble();
+	    double t=nt/bt;
+	    System.out.println("You will get "+t+" bitcoin from "+nt+" $");
+	    System.out.println(" Now Enter future rate of 1 BTC : ");
+	    double tt=sc.nextDouble();
+	    double an=(t*tt)-nt;
+	    if(tt>bt) {
+	    System.out.println("If bitcoin price goes "+tt+" than You will earn "+(an)+
+	    		"$ ");
+	    }
+	    else if(tt<bt) {
+	    	System.out.println("If bitcoin price goes "+(tt)+" than You will loose "+(-an)+
+		    		"$ ");
+	    }
+	    System.out.println("Enter "+ "y"+" to continue and "+ "n" +" to go back to the menue");
+		char p=sc.next().charAt(0);
+		if(p=='y') {
+			continue;
+		}
+		else {
+			break;
+		}
+	}
+	}
+}
+public class Project extends Thread{
+	public static void main(String[] args) {
+		//Threading
+		System.out.println("HELLO!\n");
+		try {
+		Thread.sleep(2000);
+		}
+		catch(InterruptedException e){
+			e.printStackTrace();
+		}
+		System.out.println("THERE \n");
+		try {
+			Thread.sleep(2000);
+			}
+			catch(InterruptedException e){
+				e.printStackTrace();
+			}
+		System.out.println("21SW048 \n");
+		try {
+			Thread.sleep(2000);
+			}
+			catch(InterruptedException e){
+				e.printStackTrace();
+			}//sleep method of Thread for blinking
+		Scanner sc=new Scanner(System.in);
+		System.out.println("********WELCOME TO INVESTMENT DEALINGS CALCULATOR********");
+		while(true) {
+		System.out.println("Press 1 for share investments \n"
+				+ "press 2 for property investments\n"+
+				"press 3 for knowing your interest rate\n"+
+				"press 4 for Bitcoin investments\n"+
+				"press 5 to exit\n");
 		/*when the user will enter his choice , the leading class object will be
 		 * created and it will follow it's respective code.
 		 */
@@ -152,6 +226,14 @@ public class Project{
 		else if(a==3) {
 			Display d=new Display();
 			d.run();
+		}
+		else if(a==4) {
+			bitcoinInvestment bi=new bitcoinInvestment();
+			bi.run();
+		}
+		else if(a==5) {
+			System.out.println("********THANK YOU FOR INTERACTING!***********");
+			break;
 		}
 		else {
 			System.out.println("WRONG CHOICE!");
